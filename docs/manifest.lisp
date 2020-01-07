65a0289655d304258d695b2ac899b3f559a1ea74
Nick Matvyeyev <mnasoft@gmail.com>
2020-01-07 22:41:43 +0200

    Вт янв  7 20:41:43 UTC 2020

---
+++ docs/manifest.lisp
@@ -0,0 +1,7 @@
+(:docstring-markup-format :scriba
+ :systems (:mnas-spring)
+ :documents ((:title "Mnas-Spring"
+              :authors ("Nick Matvyeyev")
+              :output-format (:type :multi-html
+                              :template :minima)
+              :sources ("manual.scr"))))
